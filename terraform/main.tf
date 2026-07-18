a11e0b8bdf133a5c44bf83cf45d63501901bc00f
Vivek Ovhal <vivek123.co@gmail.com>
2026-07-18 20:19:06 +0000

    Day 12 and Day 13: AWS Cloud Integration and Hosting your application on AWS

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,44 @@
-# 1. Tell Terraform which cloud provider you are using
 terraform {
-    required_providers {
-        aws = {
-            source "hashicorp/aws"
-            version: "~> 5.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
 }
-
-# 2 configure the AWS provider (We will use the US East Region)
 
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
-# 3. Define the actual cloud resource (A free tier Liux server)
-resource "aws_instance" "production_server" {
-    ami           = "ami-0c7217cdde317cfec"  # This is the ID for a standard Ubuntu server on AWS
-    instance_type = "t2.micro"               # This size is covered by aws FREE tier
+# 1. Create our own Custom Network (VPC)
+resource "aws_vpc" "custom_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "DevOps-Portfolio-VPC"
+  }
+}
 
-    tags = {
-        Name = "Devops-Portfolio-Server"
-    }
+# 2. Create a Subnet (a smaller slice of the network)
+resource "aws_subnet" "custom_subnet" {
+  vpc_id                  = aws_vpc.custom_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true # Ensures our server gets a public IP address
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "DevOps-Portfolio-Subnet"
+  }
+}
+
+# 3. Create the EC2 Instance and place it EXACTLY in our new Subnet
+resource "aws_instance" "production_server" {
+  ami           = "ami-0c7217cdde317cfec" 
+  instance_type = "t2.micro"              
+  subnet_id     = aws_subnet.custom_subnet.id # This line bypasses the error!
+
+  tags = {
+    Name = "DevOps-Portfolio-Server"
+  }
 }
